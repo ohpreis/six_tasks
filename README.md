ff32b52de100a881d60a43706c5e515220f7e71a
OHP <ohpreis@gmail.com>
2024-08-14 15:05:46 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+This open source project is built on the SixTasks methodology. For more information on how the SixTasks method works, please refer to the [documentation](https://ohp.bearblog.dev/six-tasks-productivity-method/).
+
+Stay tuned for future updates!
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
+  end
+end
