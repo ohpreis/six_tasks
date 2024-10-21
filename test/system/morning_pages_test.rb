4e390a7de5797b98667aa1994ef478d85e26d422
OHP <ohpreis@gmail.com>
2024-10-21 17:40:44 +0000

    Same for mp

--- test/system/morning_pages_test.rb
+++ test/system/morning_pages_test.rb
@@ -10,25 +10,13 @@ class MorningPagesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Morning pages"
   end
 
-  test "should create morning page" do
-    visit morning_pages_url
-    click_on "New morning page"
-
-    fill_in "Body", with: @morning_page.body
-    fill_in "User", with: @morning_page.user_id
-    click_on "Create Morning page"
-
-    assert_text "Morning page was successfully created"
-    click_on "Back"
-  end
-
   test "should update Morning page" do
     visit morning_page_url(@morning_page)
     click_on "Edit this morning page", match: :first
 
     fill_in "Body", with: @morning_page.body
     fill_in "User", with: @morning_page.user_id
-    click_on "Update Morning page"
+    click_on "Update"
 
     assert_text "Morning page was successfully updated"
     click_on "Back"
