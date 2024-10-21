f6db5eca7a0b8f8760f10db42400a9236e0423bf
OHP <ohpreis@gmail.com>
2024-10-21 18:10:30 +0000

    sh...

--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -4,19 +4,4 @@ class TasksTest < ApplicationSystemTestCase
   setup do
     @task = tasks(:one)
   end
-
-  test "visiting the index" do
-    visit tasks_url
-    assert_selector "h1", text: "Tasks"
-  end
-
-
-  test "should update Task" do
-    visit task_url(@task)
-
-    click_on "Update"
-
-    assert_text "Task was successfully updated"
-    click_on "Back"
-  end
 end
