c919e4eaea35923f73d9789287463bfe081580f3
OHP <ohpreis@gmail.com>
2024-10-23 20:58:47 +0000

    Updated morning pages page.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,9 +13,20 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        base: '1.125rem', // 18px, default is 1rem (16px)
-        lg: '1.25rem',    // 20px, default is 1.125rem (18px)
-        xl: '1.5rem',     // 24px, default is 1.25rem (20px)
+        xs: '0.75rem',    // 12px
+        sm: '0.875rem',   // 14px
+        base: '1rem',     // 16px
+        lg: '1.125rem',   // 18px
+        xl: '1.25rem',    // 20px
+        '2xl': '1.5rem',  // 24px
+        '3xl': '1.875rem',// 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem',    // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem',  // 72px
+        '8xl': '6rem',    // 96px
+        '9xl': '8rem',    // 128px
+        'mpages': '1.2rem', // morning pages big screen
       },
     },
   },
